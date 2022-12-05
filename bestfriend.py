e01e471fb5c38eb6b4aa2d599d23418669f09599
Matthew Wemple <wemplematt@gmail.com>
2022-12-05 13:10:37 -0500

    first wierd commit#

--- bestfriend.py
+++ bestfriend.py
@@ -1,1 +1,5 @@
-print("hello world!")
+first_name = input("What is your name?")
+print(f"Hello {first_name}")
+best = input("Who is the best?")
+print(f"NO! {best} is not the best, I am the best!")
+
